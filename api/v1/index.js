96d25296f9a4f588594cbebf1a2c0ffcd6f4d697
thun888 <2238342947@qq.com>
2025-09-02 23:46:44 +0800

    upgrade to nodejs 22

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,233 +1,207 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+import https from "node:https";
+import { JSDOM } from "jsdom";
+
 const HOSTS = process.env.HOSTS || ['localhost'];
-
-// /api/v1?url=https://xaoxuu.com
-var https = require('https');
-var cache = {};
-
-export default function handler(req, res) {
-    const referer = req.headers.referer || '';
-    if (referer.length > 0) {
-      const refererHost = new URL(referer).hostname || '';
-      if (!HOSTS.includes(refererHost)) {
-          console.error('referer invalid:', referer);
-          res.send({"title": "请自部署该服务", "desc": "https://github.com/xaoxuu/site-info-api/"});
-          return;
-      }
-    } else {
-      if (!HOSTS.includes('')) {
-          console.error('referer can not be empty!');
-          res.send({"title": "请自部署该服务", "desc": "https://github.com/xaoxuu/site-info-api/"});
-          return;
-      }
+const cache = {};
+export default async function handler(req, res) {
+  const referer = req.headers.referer || "";
+  if (referer.length > 0) {
+    const refererHost = new URL(referer).hostname || "";
+    if (!HOSTS.includes(refererHost)) {
+      console.error("referer invalid:", referer);
+      res.json({
+        title: "请自部署该服务",
+        desc: "https://github.com/xaoxuu/site-info-api/",
+      });
+      return;
     }
-    console.log('referer ok:', referer);
-
-    console.log('prams url:', req.url);
-
+  } else {
+    if (!HOSTS.includes("")) {
+      console.error("referer can not be empty!");
+      res.json({
+        title: "请自部署该服务",
+        desc: "https://github.com/xaoxuu/site-info-api/",
+      });
+      return;
+    }
+  }
+  console.log("referer ok:", referer);
     // 参数 type url
     // type 不存在默认是 site
     // type: site | voice | file
     // url: 网址
+  const validTypes = ["site", "voice", "file"];
+  const type = req.query?.type || "site";
+  if (!validTypes.includes(type)) {
+    console.log("type invalid:", type);
+    res.json({});
+    return;
+  }
+  console.log("type:", type);
 
-    const validTypes = ['site', 'voice', 'file'];
+  const url = req.query?.url || "";
+  if (!url.startsWith("http")) {
+    console.error("url invalid:", url);
+    res.json({});
+    return;
+  }
+  console.log("url:", url);
 
-    const type = req.query['type'] || 'site';
-    if (!validTypes.includes(type)) {
-        console.log('type invalid:', type);
-        res.send({});
-        return;
-    }
-    console.log('type:', type);
+  res.setHeader("Vercel-CDN-Cache-Control", "max-age=604800");
 
-    const url = req.query['url'] || '';
-    if (!url.startsWith('http')) {
-        console.error('url invalid:', url);
-        res.send({});
-        return;
-    }
-    console.log('url:', url);
-    //添加头
-    res.setHeader("Vercel-CDN-Cache-Control", "max-age=604800");
-    if (cache[type + ' ' + url]) {
-        console.log('use cache');
-        res.send(cache[type + ' ' + url]);
-    } else {
-        switch(type) {
-            case 'site':
-                main(url, (data) => {
-                    if (Object.keys(data).length > 0) {
-                        data.url = url;
-                        cache['site ' + url] = data;
-                    }
-                    res.send(data);
-                });
-                break;
-            case 'voice':
-                break;
-            case 'file':
-                getFile(url, (data) => {
-                    if (Object.keys(data).length > 0) {
-                        data.url = url;
-                        cache['file ' + url] = data;
-                    }
-                    console.log(data);
-                    res.send(data);
-                });
-                break;
-            default:
-                console('can not exist!');
+  const cacheKey = `${type} ${url}`;
+  if (cache[cacheKey]) {
+    console.log("use cache");
+    res.json(cache[cacheKey]);
+    return;
+  }
+
+  switch (type) {
+    case "site":
+      main(url, (data) => {
+        if (Object.keys(data).length > 0) {
+          data.url = url;
+          cache[cacheKey] = data;
         }
-    }
+        res.json(data);
+      });
+      break;
+    case "voice":
+      res.json({});
+      break;
+    case "file":
+      getFile(url, (data) => {
+        if (Object.keys(data).length > 0) {
+          data.url = url;
+          cache[cacheKey] = data;
+        }
+        res.json(data);
+      });
+      break;
+    default:
+      console.error("can not exist!");
+      res.json({});
+  }
 }
 
 function main(url, callback) {
-    const request = https.get(url, (response) => {
-        let html = '';
-        response.on('data', (chunk) => {
-            html = html + chunk.toString();
-        });
-        response.on('end', () => {
-            console.log('end:', response.statusCode);
-            if (response.statusCode != 200) {
-                let location = response.headers['location'];
-                let isRedirect = [301,302,303,307,308].includes(response.statusCode);
-                console.log('isRedirect:', isRedirect);
-                console.log('location:', location);
-                if (isRedirect && location && location != url) {
-                    main(location, callback);
-                    return;
-                } else {
-                    callback({});
-                    return;
-                }
-            }
-            getInfo(url, html, (data) => {
-                console.log('data:', data);
-                callback(data);
-            });
-        });
+  const request = https.get(url, (response) => {
+    let html = "";
+    response.on("data", (chunk) => {
+      html += chunk.toString();
     });
-    request.on('error', error => {
-        console.error('error:', error);
-        callback({});
-    })
-    request.end();
+    response.on("end", () => {
+      console.log("end:", response.statusCode);
+      if (response.statusCode !== 200) {
+        let location = response.headers["location"];
+        let isRedirect = [301, 302, 303, 307, 308].includes(response.statusCode);
+        if (isRedirect && location && location !== url) {
+          main(location, callback);
+          return;
+        } else {
+          callback({});
+          return;
+        }
+      }
+      getInfo(url, html, (data) => {
+        callback(data);
+      });
+    });
+  });
+  request.on("error", (error) => {
+    console.error("error:", error);
+    callback({});
+  });
+  request.end();
 }
-
 /**
  * Determine if it is a ['https://', 'http://', '//'] protocol
  * @param {String} url Website url
  * @returns {Boolean}
  */
 function isHttp(url) {
-    return /^(https?:)?\/\//g.test(url)
+  return /^(https?:)?\/\//g.test(url);
 }
 
 function getInfo(link, html, callback) {
-    try {
-        let data = {};
-        let title, icon, desc;
-        const { document } = (new JSDOM(html)).window;
-        
-        // title
-        let elTitle = document.querySelector('title');
-        if (!elTitle) {
-            elTitle = document.querySelector('head meta[property="og:title"]');
-        }
-        if (elTitle) {
-            title = elTitle.text || elTitle.content;
-        }
-        if (title) {
-            data.title = title;
-        }
+  try {
+    let data = {};
+    let title, icon, desc;
+    const { document } = new JSDOM(html).window;
 
-        // desc
-        let elDesc = document.querySelector('head meta[property="og:description"]');
-        if (!elDesc) {
-            elDesc = document.querySelector('head meta[name="description"]');
-        }
-        if (elDesc) {
-            desc = elDesc.content;
-        }
-        if (desc) {
-            data.desc = desc;
-        }
+    // title
+    let elTitle = document.querySelector("title") ||
+                  document.querySelector("head meta[property='og:title']");
+    if (elTitle) title = elTitle.text || elTitle.content;
+    if (title) data.title = title;
 
-        // icon
-        let elIcon = document.querySelector('head link[rel="apple-touch-icon"]');
-        if (!elIcon) {
-            elIcon = document.querySelector('head link[rel="icon"]')
-        }
-        if (elIcon) {
-            icon = elIcon && elIcon.getAttribute('href');
-        } else {
-            elIcon = document.querySelector('head meta[property="og:image"]');
-            if (!elIcon) {
-                elIcon = document.querySelector('head meta[property="twitter:image"]');
-            }
-            if (elIcon) {
-                icon = elIcon.content;
-            }
-        }
-        
-        if (/^data:image/.test(icon)) {
-            icon = '';
-        }
+    // desc
+    let elDesc = document.querySelector("head meta[property='og:description']") ||
+                 document.querySelector("head meta[name='description']");
+    if (elDesc) desc = elDesc.content;
+    if (desc) data.desc = desc;
 
-        // If there is no src then get the site icon
-        if (!icon) {
-            const links = [].slice.call(document.querySelectorAll('link[rel][href]'))
-            elIcon = links.find((_el) => _el.rel.includes('icon'))
-            icon = elIcon && elIcon.getAttribute('href')
-        }
-
-        // If `icon` is not the ['https://', 'http://', '//'] protocol, splice on the `origin` of the a tag
-        if (icon && !isHttp(icon)) {
-            icon = new URL(link).origin + icon;
-        }
-        if (icon) {
-            data.icon = icon;
-        }
-
-        callback(data);
-    } catch (error) {
-        console.log('error >>', error);
-        callback({});
+    // icon
+    let elIcon =
+      document.querySelector("head link[rel='apple-touch-icon']") ||
+      document.querySelector("head link[rel='icon']");
+    if (elIcon) {
+      icon = elIcon.getAttribute("href");
+    } else {
+      elIcon =
+        document.querySelector("head meta[property='og:image']") ||
+        document.querySelector("head meta[property='twitter:image']");
+      if (elIcon) icon = elIcon.content;
     }
+
+    if (/^data:image/.test(icon)) icon = "";
+
+    if (!icon) {
+      const links = [...document.querySelectorAll("link[rel][href]")];
+      elIcon = links.find((_el) => _el.rel.includes("icon"));
+      icon = elIcon && elIcon.getAttribute("href");
+    }
+
+    if (icon && !isHttp(icon)) {
+      icon = new URL(link).origin + icon;
+    }
+    if (icon) data.icon = icon;
+
+    callback(data);
+  } catch (error) {
+    console.log("error >>", error);
+    callback({});
+  }
 }
 
 function getFile(url, callback) {
-    const request = https.get(url, (response) => {
-        response.setEncoding('binary') // 二进制
-        let file = '';
-        response.on('data', (chunk) => {
-            file = file + chunk;
-        });
-        response.on('end', () => {
-            console.log('end:', response.statusCode);
-            if (response.statusCode != 200) {
-                let location = response.headers['location'];
-                let isRedirect = [301,302,303,307,308].includes(response.statusCode);
-                console.log('isRedirect:', isRedirect);
-                console.log('location:', location);
-                if (isRedirect && location && location != url) {
-                    getFile(location, callback);
-                    return;
-                } else {
-                    callback({});
-                    return;
-                }
-            }
-            let data = {};
-            data.file = Buffer.from(file, 'binary');
-            callback(data);
-        });
+  const request = https.get(url, (response) => {
+    response.setEncoding("binary"); // 二进制
+    let file = "";
+    response.on("data", (chunk) => {
+      file += chunk;
     });
-    request.on('error', error => {
-        console.error('error:', error);
-        callback({});
-    })
-    request.end();
+    response.on("end", () => {
+      console.log("end:", response.statusCode);
+      if (response.statusCode !== 200) {
+        let location = response.headers["location"];
+        let isRedirect = [301, 302, 303, 307, 308].includes(response.statusCode);
+        if (isRedirect && location && location !== url) {
+          getFile(location, callback);
+          return;
+        } else {
+          callback({});
+          return;
+        }
+      }
+      let data = {};
+      data.file = Buffer.from(file, "binary");
+      callback(data);
+    });
+  });
+  request.on("error", (error) => {
+    console.error("error:", error);
+    callback({});
+  });
+  request.end();
 }
